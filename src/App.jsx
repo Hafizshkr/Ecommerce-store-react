0df1b47e4c8ca3963fe077db1da9253baeed7866
Hafizshkr <103745630+Hafizshkr@users.noreply.github.com>
2024-05-29 18:50:09 +0800

    add single page product with router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./Pages/Homepage/Homepage";
 import AboutUs from "./Pages/About us/AboutUs";
 import Store from "./Pages/Store/Store";
 import NotFound from "./Pages/404/NotFound";
+import SinglePage from "./Pages/Single Page/SinglePage";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +17,7 @@ const App = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/About" element={<AboutUs />} />
           <Route path="/Store" element={<Store />} />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="/SinglePage/:productId" element={<SinglePage />} />
         </Routes>
       </Router>
       </QueryClientProvider>
