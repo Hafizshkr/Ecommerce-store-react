e4597a4eba7b74deb3e82f6111ef2523d2109e28
Hafizshkr <103745630+Hafizshkr@users.noreply.github.com>
2024-08-22 17:14:52 +0800

    updated layout cart page

--- src/Pages/Cart/CartPage.jsx
+++ src/Pages/Cart/CartPage.jsx
@@ -7,32 +7,37 @@ const CartPage = () => {
   return (
     <>
       <Navbar />
-      <div className="flex align-center justify-center min-h-screen bg-red-100">
+      <div className="flex align-center justify-center min-h-screen ">
         <div className="flex align-center  flex-col">
           <div>
             <h1 className="text-center py-5 text-[34px]">Cart Items</h1>
           </div>
-          <div className="px-5 py-5 h-[600px] w-[600px] bg-green-200">
+          <div className="px-5 py-5  w-[1200px]">
             {cartItems.length === 0 ? (
               <p>Your cart is empty</p>
             ) : (
-              <table>
-                <tr>
-                  <th>Items</th>
-                  <th>Price</th>
-                  <th>Qty</th>
-                  <th>Total</th>
+              <table className="w-full h-full ">
+                <tr >
+                  <th className="px-5 pb-2 border border-black border-b-2 border-0 \">Product</th>
+                  <th className="px-5 pb-2 border border-black border-b-2 border-0">Unit Price</th>
+                  <th className="px-5 pb-2 border border-black border-b-2 border-0">Qty</th>
+                  <th className="px-5 pb-2 border border-black border-b-2 border-0">Total</th>
                 </tr>
-                <tr>
+                <tbody>
                   {cartItems.map((item) => (
-                    <tr key={item.productId}>
-                      <td>{item.productTitle}</td>
-                      <td>{item.productPrice}</td>
-                      <td>{item.quantity}</td>
-                      <td>{item.quantity * item.productPrice}</td>
+                    <tr key={item.productId} className="h-[100px]">
+                      <td className="px-5 py-5 gap-5  flex  justify-center   items-center">
+                        <img src={item.productImage} className="w-[80px] h-[80px]"/>
+                        {item.productTitle}
+                      </td>
+                      <td className="px-5 py-5 text-center">RM{item.productPrice}</td>
+                      <td className="px-5 py-5 text-center">{item.quantity}</td>
+                      <td className="px-5 py-5 text-center">
+                      RM{item.quantity * item.productPrice}
+                      </td>
                     </tr>
                   ))}
-                </tr>
+                </tbody>
               </table>
             )}
           </div>
