9d6daa2e6c23f2112a7d95fe227cc3e90a441d86
Hafizshkr <103745630+Hafizshkr@users.noreply.github.com>
2024-08-23 18:01:36 +0800

    added adjustable quantity in the cart page

--- src/Pages/Store/Cart/cart.js
+++ src/Pages/Store/Cart/cart.js
@@ -35,12 +35,40 @@ const cartSlice = createSlice({
       const productId = action.payload;
       state.items = state.items.filter((item) => item.productId !== productId);
       state.totalPrice = state.items.reduce(
-        (total,item) => total + item.quantity * item.productPrice, 0
-      )
+        (total, item) => total + item.quantity * item.productPrice,
+        0
+      );
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+    quantifierPlus(state, action) {
+      const { productId, quantity } = action.payload;
+      const indexProductId = state.items.findIndex(
+        (items) => items.productId === productId
+      );
+      if (indexProductId >= 0) {
+        state.items[indexProductId].quantity += quantity;
+      }
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+    quantifierMinus(state, action) {
+      const { productId, quantity } = action.payload;
+      const indexProductId = state.items.findIndex(
+        (items) => items.productId === productId
+      );
+      if (indexProductId >= 0) {
+        if (state.items[indexProductId].quantity > quantity) {
+          state.items[indexProductId].quantity -= quantity;
+        } else {
+          state.items = state.items.filter(
+            (item) => item.productId !== productId
+          );
+        }
+      }
       localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
 
 export default cartSlice.reducer;
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, quantifierPlus, quantifierMinus } =
+  cartSlice.actions;
